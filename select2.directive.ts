c81a9dd76f48ca703ebd4331bbb1a669b339f816
hasanenalbana <hasanen@gmail.com>
2018-01-20 23:18:21 +0300

    using [(ngModel)] instead of [value]

--- select2.directive.ts
+++ select2.directive.ts
@@ -1,7 +1,6 @@
 import {Directive,  ElementRef,  OnChanges, Input} from '@angular/core';
 import {NgControl} from "@angular/forms";
 
-
 declare var require: any
 const $ = require('jquery');
 
@@ -11,8 +10,8 @@ const $ = require('jquery');
 
 
 export class Select2Directive implements OnChanges{
-
-    @Input() value: any;
+    @Input() ngModel: any;
+    private value: any;
     public multiple: boolean = false;
     private elementId: string;
     private select2: any = null;
@@ -23,6 +22,7 @@ export class Select2Directive implements OnChanges{
 
     ngAfterViewInit(){
         this.elementId = this.el.nativeElement.id;
+        this.multiple = this.el.nativeElement.multiple;
         if (! $('#'+this.elementId).hasClass("select2-hidden-accessible")) {
             this.select2 = $('#'+this.elementId).select2();
             this.select2.val(null).trigger('change');
@@ -30,19 +30,28 @@ export class Select2Directive implements OnChanges{
         }
     }
 
+    removeEmptyValues(): void{
+        if (!this.value) return;
+        let i = this.value.indexOf("");
+        if (i>=0)
+            this.value.splice(i,1);
+    }
+
     subscribeToChanges(): void{
         $('#'+this.elementId).on("select2:select",(ev: any) => {
             let id: any = ev['params']['data'].id;
-            this.value = id;
-            this.ngControl.control.patchValue(id);
-        });
-
+                this.value = id;
+                this.ngControl.control.patchValue(id);
+                this.ngModel = id;
+            });
     }
 
     ngOnChanges(changes){
-        if (changes.value){
-            this.value = changes.value.currentValue;
+        if (changes.ngModel){
+            this.value = changes.ngModel.currentValue;
             $('#'+this.elementId).select2('val',[this.value]);
         }
     }
+
+
 }
